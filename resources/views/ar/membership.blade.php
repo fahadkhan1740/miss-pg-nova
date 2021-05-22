97a8ffff0a5404953fd5bf1f53e9192a3be6d4c1
pixelcode <pixelcodeworks@gmail.com>
2021-05-22 16:33:27 +0530

    Memberships | Image fixed size

--- resources/views/ar/membership.blade.php
+++ resources/views/ar/membership.blade.php
@@ -2,14 +2,14 @@
 
     <!-- breadcrumb area -->
     <div class="breadcrumb-style-1 about-breadcrumb-overlay"
-         style="background-image:url({{ 'storage/'.getBanner('memberships') }});"
-    >
+        style="background-image:url({{ 'storage/'.getBanner('memberships') }});"
+        >
         <div class="breadcrumb-inner">
             <h1 class="page-title">{{ __('custom.links.memberships_packages') }}</h1>
             <ul class="page-list">
                 <li><a href="{{ route('home', ['locale' => 'ar']) }}">{{ __('custom.links.home') }}</a></li>
                 <li>
-                    <a href="{{ route('memberships', ['locale' => app()->getLocale()]) }}">{{ __('custom.links.memberships_packages') }}</a>
+<a href="{{ route('memberships', ['locale' => app()->getLocale()]) }}">{{ __('custom.links.memberships_packages') }}</a>
                 </li>
             </ul>
         </div>
@@ -20,46 +20,46 @@
     <div class="memberships-area padding-top-90 padding-bottom-200">
         <div class="container">
             @foreach ($memberships as $membership)
-                @if ($loop->iteration % 2 !== 0)
-                    <div class="row no-gutter">
-                        <div class="col-lg-6">
-                            <img src="{{ 'storage/'.$membership->image }}">
-                        </div>
-                        <div class="col-lg-6 pl-5 pt-3">
-                            <h4 class="text-success">{{ $membership->title_ar }}</h4>
-                            <p class="text-left">
-                                {!! $membership->short_description_ar !!}
-                            </p>
-                            <p class="text-left">
-                            <ul>
-                                @foreach(explode(',',$membership->features_ar) as $feature)
-                                    <li>
-                                        {{ trim($feature) }}
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                @else
-                    <div class="row no-gutter">
-                        <div class="col-lg-6 pr-5 pt-3">
-                            <h4 class="text-success">{{ $membership->title_ar }}</h4>
-                            <p class="text-right">
-                                {!! $membership->short_description_ar !!}
-                            </p>
-                            <ul>
-                                @foreach(explode(',',$membership->features_ar) as $feature)
-                                    <li>
-                                        {{ trim($feature) }}
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <img src="https://dummyimage.com/585x410/bdb9bd/474747.png">
-                        </div>
-                    </div>
-                @endif
+            @if ($loop->iteration % 2 !== 0)
+            <div class="row no-gutter">
+                <div class="col-lg-6">
+                    <img src="{{ 'storage/'.$membership->image }}" style="width: inherit">
+                </div>
+                <div class="col-lg-6 pl-3 pt-3">
+                    <h4 class="text-success">{{ $membership->title_ar }}</h4>
+                    <p class="text-left">
+                        {!! $membership->short_description_ar !!}
+                    </p>
+                    <p class="text-left">
+                        <ul>
+                            @foreach(explode(',',$membership->features_ar) as $feature)
+                            <li>
+                                {{ trim($feature) }}
+                            </li>
+                            @endforeach
+                        </ul>
+                </div>
+            </div>
+            @else
+            <div class="row no-gutter">
+                <div class="col-lg-6 pr-5 pt-3">
+                    <h4 class="text-success">{{ $membership->title_ar }}</h4>
+                    <p class="text-right">
+                        {!! $membership->short_description_ar !!}
+                    </p>
+                    <ul>
+                        @foreach(explode(',',$membership->features_ar) as $feature)
+                        <li>
+                            {{ trim($feature) }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <img src="{{ 'storage/'.$membership->image }}" style="width: inherit">
+                </div>
+            </div>
+            @endif
             @endforeach
             <div class="row padding-top-100">
                 <div class="col-lg-12 text-center">
