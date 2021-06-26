e3796c874221f31132dda377b614a4e01dd6d4ce
Fahad Khan <fahadkhan1740@outlook.com>
2021-06-26 22:17:59 +0300

    remove branches from search

--- app/Nova/Branch.php
+++ app/Nova/Branch.php
@@ -41,9 +41,7 @@ class Branch extends Resource
      *
      * @var array
      */
-    public static $search = [
-        'id','name'
-    ];
+    public static $search = [];
 
     /**
      * Get the fields displayed by the resource.
